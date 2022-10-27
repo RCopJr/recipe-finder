7fa33a7f98643275a230e4e73e9e358268f620ad
Ramonito Copreros <rjcoprer@uwaterloo.ca>
2022-10-27 18:59:31 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("home", { api_key: process.env.API_KEY });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
+});
