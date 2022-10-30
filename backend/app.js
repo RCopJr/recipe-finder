ec299d7b3a930f5f8c188f424134f2839a5498e1
Ramonito Copreros <rjcoprer@uwaterloo.ca>
2022-10-30 17:14:53 -0400

    Added basic form and setup API call to return recipe title and url

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get("/test", function (req, res) {
-  res.json({ testData: "estsetse" });
+app.get("/search", function (req, res) {
+  const search = req.query.search;
+  axios
+    .get("https://api.spoonacular.com/recipes/complexSearch", {
+      params: {
+        apiKey: process.env.API_KEY,
+        number: 1,
+        maxCalories: 500,
+        addRecipeInformation: true,
+        query: search,
+      },
+    })
+    .then((response) => {
+      const result = response.data.results[0];
+      const title = result.title;
+      const sourceUrl = result.sourceUrl;
+      res.json({ title: title, url: sourceUrl });
+    });
 });
 
 app.listen(5000, function () {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,102 @@
-import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  TextField,
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: "http://localhost:3000/",
+});
 
 function App() {
-  const [testData, setTestData] = useState("");
+  const [search, setSearch] = useState("");
+  const [recipe, setRecipe] = useState({
+    title: "",
+    url: "",
+  });
 
-  useEffect(() => {
-    fetch("/test")
-      .then((response) => response.json())
-      .then((data) => {
-        setTestData(data.testData);
-        console.log(testData);
+  function handleSearch(event) {
+    setSearch(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    client
+      .get("/search", {
+        params: {
+          search: search,
+        },
+      })
+      .then((res) => {
+        setRecipe((prevRecipe) => ({
+          ...prevRecipe,
+          title: res.data.title,
+          url: res.data.url,
+        }));
       });
-  }, []);
+  }
 
   return (
-    <div className="App">
-      <h1>{testData}</h1>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={4} alignItems="center" justify="center">
+          <Grid item xs={8}>
+            <TextField
+              onChange={handleSearch}
+              name="search"
+              label="Search field"
+              type="search"
+              variant="filled"
+              value={search}
+              fullWidth // Used to take up full width of container
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <Button type="submit" variant="contained">
+              Go
+            </Button>
+          </Grid>
+          {recipe.title && recipe.url && (
+            <Grid item xs={8}>
+              <Card variant="outlined">
+                <CardContent>
+                  <CardContent>
+                    <Typography
+                      sx={{ fontSize: 14 }}
+                      color="text.secondary"
+                      gutterBottom
+                    >
+                      {recipe.title}
+                    </Typography>
+                    <a href={recipe.url}>
+                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        See recipe here.
+                      </Typography>
+                    </a>
+                  </CardContent>
+                </CardContent>
+              </Card>
+            </Grid>
+          )}
+        </Grid>
+      </form>
     </div>
   );
 }
 
 export default App;
+// useEffect(() => {
+//   // client.get("/test").then((response) => console.log(response.data));
+//   client
+//     .post("", {
+//       data: "poop",
+//     })
+//     .then((response) => {
+//       console.log("Worked");
+//     });
+// }, []);
